1fa085442aa71bbe519ce06ffdbc4e81b05bb781
megha-ml-ds-neural <jainmegha835@gmail.com>
2020-06-08 12:53:24 +0530

    day 7

---
+++ javascrpt_tutorials/01webbasics/myscipt.js
@@ -0,0 +1,21 @@
+// alert('File is touched')
+
+
+// I got pop-up on terminal
+
+
+
+// console.log(document.title);
+// this print on console on browser
+document.title="i got changed";
+console.log(document.title);
+// from this,i changed title
+
+
+document.querySelector('button').addEventListener('click',()=>{
+    console.log('button was pressed');
+})
+// button is created
+
+
+
